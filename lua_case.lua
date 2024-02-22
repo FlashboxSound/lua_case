9411a739c1a3787d73d00f8f22bf5f4ccaa4caea
Flashbox <kalle.frosta@gmail.com>
2024-02-22 18:14:19 +0100

    Day 4_update
    - Last bugg fixes
    - Make everything prettier

--- lua_case.lua
+++ lua_case.lua
@@ -1,4 +1,18 @@
--- Test constants:
+-- LUA CASE FOR STORYKIT_ APPLICATION
+-- By André Johanssson
+
+-- Code conventions:
+-- All variables are formated in snake_case according to the variables given in the case (ie lang_id)
+-- All function-names are written in CamelCase
+-- Indentation is keept at 4 spaces according to LUA guidelines (they speek of 2 to 4 spaces)
+-- Itterators local variables are named according to most fitting for their function or k, v for the outer loop and i, j for the inner loop
+-- Global variables are avoided
+
+
+
+
+
+-- Given data in the case
 local input = {
     greeting = {
         eng = "Hello World!",
@@ -34,24 +48,25 @@ local lang_id = "swe"
 
 
 
--- Helper functions:
-local function PrintArray(tbl)
+
+
+-- Helper functions and variables
+local string_spacer = "\n=   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =   =\n\n"
+local string_tab = "    "
+
+local function PrintArray(tbl) -- Prints an array
     for _, v in ipairs(tbl) do
-        print(v)
+        io.write(string_tab .. v .. "\n")
     end
 end
 
-
--- Function to shorten any given text if it's longer than the given cap
-local function ShortenTextIfRequired(text, capAtLen)
-    if string.len(text) > capAtLen then
-        return string.sub(text, 1, capAtLen - 3) .. "..."
+local function ShortenTextIfRequired(text, cap_at_len) -- Function to shorten any given text if it's longer than the given cap
+    if string.len(text) > cap_at_len then
+        return string.sub(text, 1, cap_at_len - 3) .. "..."
     else
         return nil
     end
 end
-
-
 
 local ChooseLanguangeID = function (choice) -- Switch case (C) style implementation of a language choice to avoid enless if-statments and allow for a default value
     choice = string.lower(choice) -- Make sure that the input is read even with different formating
@@ -74,7 +89,7 @@ local ChooseLanguangeID = function (choice) -- Switch case (C) style implementat
         end,
 
         default = function ( )
-            io.write("\nYour choice is din't match any of the specified languanges, the program will default to: lorem")
+            io.write("\nYour choice is din't match any of the specified languanges, the program will default to language: lorem")
             lang_id = "lorem"
         end,
         }
@@ -88,21 +103,24 @@ local ChooseLanguangeID = function (choice) -- Switch case (C) style implementat
 end
 
 
--- Input module for picking a language ID. All reading and printing is done directly in the terminal. This is soley for testing purposes.
 
+
+
+-- Input module for picking a language ID. All reading and printing is done directly in the terminal.
 local answer
 io.write("Choose language ID (eng, swe, ger or lorem): ")
 answer = io.read()
 
 ChooseLanguangeID(answer)
+ChooseLanguangeID(lang_id) -- Make sure that the language ID is not nil or a non exsiting languange before going further, if so default to lorem
 
-io.write("\n\n= = = = = = = = = = = = = = = = = = = =\n\n")
+io.write("\n" .. string_spacer)
 
 
 
--- Task: Create an array table where the strings are stored and ordered by text_id from lowest to highest id
--- Description: ...
 
+
+-- Creating the output table with correct structure to fill in later
 local output = {
     greeting = {
         value = "",
@@ -120,11 +138,10 @@ local output = {
         value = "",
         text_id = 0
     }
-} -- Creating the output table
+}
 
-ChooseLanguangeID(lang_id) -- Make sure that the language ID is not nil or a non exsiting languange, if so default to lorem
-
-local function ExtractLanguage(input_tbl, output_tbl, language) -- A function to map an existing tables values to a new table given a filter
+-- Maps the values from the input table to the corresponding spot in the output table after filtering out bassed on lang_id
+local function ExtractLanguage(input_tbl, output_tbl, language)
     for element_type, element_table in pairs(input_tbl) do
         for key, value in pairs(element_table) do
                 if key == language then
@@ -136,6 +153,8 @@ local function ExtractLanguage(input_tbl, output_tbl, language) -- A function to
     end
 end
 
+-- Proccesses the filtered and mapped table to see if any strings are longer then 20 chars.
+-- If so creates a new key value pair [shortened_text] and the text shortened to 17 chars with "..." concatenated to the end
 local function ProccessTable(tbl)
     for element_type, element_table in pairs(tbl) do
         for key, value in pairs(element_table) do
@@ -147,33 +166,29 @@ local function ProccessTable(tbl)
     end
 end
 
+-- Do the extraction and proccessing
 ExtractLanguage(input, output, lang_id)
 ProccessTable(output)
 
-local function PrintOutput(tbl) -- Function to handle printing of the output table and all its key-value pairs
+local function PrintOutput(tbl) -- A function to handle printing of the output table and all its key-value pairs in correct formating
     for k, v in pairs(tbl) do
         local headline = k
-        io.write(headline .. ":\n")
+        io.write(string_tab .. headline .. ":\n")
         for i, j in pairs(v) do
             local key = i
             local value = j
-            io.write("\t" .. key .. ": " .. value .. "\n")
+            io.write(string_tab .. string_tab .. key .. ": " .. value .. "\n")
         end
     end
 end
 
-io.write("Finalized output table:\n") -- Print the finalized output table
-
+io.write("Finalized output table:\n")
 PrintOutput(output)
-io.write("\n= = = = = = = = = = = = = = = = = = = =\n\n")
+io.write(string_spacer)
 
-
-
--- Task: Create an array table where the strings are stored and ordered by text_id from lowest to highest id
--- Description: ...
-
-function TableToAssositiveArray(tbl, order) -- Function that transforms a table with multiple levels to a flat, associative array with the key [text_id] and value [value]
-    local new_table = {}
+-- A function that maps the values: [text_id] and [value] to form a new key pair in an associative array
+local function TableToAssositiveArray(tbl)
+    local new_tbl = {}
     local position = 0
     local string = ""
 
@@ -185,28 +200,20 @@ function TableToAssositiveArray(tbl, order) -- Function that transforms a table 
                 string = j
             end
         end
-        new_table[position] = string -- Places the new pairs at the correct position according to the [text_id], no sorting needed
+        new_tbl[position] = string -- Places the new pairs at the correct position according to the [text_id], no sorting needed
     end
-
-    return new_table
+    return new_tbl
 end
 
 local ordered_by_id = TableToAssositiveArray(output)
 
 io.write("Ordered by text ID:\n")
-
 PrintArray(ordered_by_id)
+io.write(string_spacer)
 
-io.write("\n= = = = = = = = = = = = = = = = = = = =\n\n")
-
-
-
--- Task: Create an array table where the strings are stored and sorted from fewest characters to most characters
--- Description: ...
-
+-- Takes the assocciative array of [text_id] [value] pairs and orders them in order of shortest string to longest
 local ordered_by_lenght = ordered_by_id
 table.sort(ordered_by_lenght, function(a,b) return #a<#b end)
 
 io.write("Ordered by string lenght:\n")
-
 PrintArray(ordered_by_lenght)
